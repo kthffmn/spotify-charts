f154a382f1a7ee13c3c1f02d200a641ae8583dd9
Katie Hoffman <ktahoffman@gmail.com>
2016-04-13 20:32:36 -0400

    getting a url parsing error in addition to an input stream error

--- src/main/java/SpotifyCharts.java
+++ src/main/java/SpotifyCharts.java
@@ -3,44 +3,79 @@
  */
 
 import java.util.*;
+import java.util.stream.*;
 import com.google.common.collect.*;
+import java.io.*;
+import java.net.*;
+import javax.json.*;
 
 public class SpotifyCharts {
 
-    private static final Map<Integer,String> SHAPES = ImmutableMap.of(
-        3, "triangle",
-        4, "quadrilateral",
-        5, "pentagon"
+    private String url = "https://spotifycharts.com/api/";
+
+    private static final Map<String,String> PARAMS = ImmutableMap.of(
+        "type", "regional",
+        "country", "US",
+        "recurrence", "daily",
+        "date", "latest"
     );
 
-    public static Optional<String> getShape(int numOfSides) {
-        String shape = SHAPES.get(numOfSides);
-        if (shape == null) {
-          return Optional.empty();
+    private String country;
+    private int rank;
+
+    public SpotifyCharts(final String country, final int rank) {
+        this.country = country;
+        this.rank = rank;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getParams() {
+        String finalParams = PARAMS.entrySet().stream()
+                .map(Object::toString)
+                .collect(Collectors.joining("&"));
+        return finalParams;
+    }
+
+    public String getStringUrl() {
+        String params = getParams();
+        System.out.println(url + "?" + params);
+        return url + "?" + params;
+    }
+
+    public URL getUrl() {
+        String sUrl = getStringUrl();
+        System.out.println(sUrl);
+        return new URL(getStringUrl());
+    }
+
+    public JsonArray getSongs() {
+        JsonArray songs;
+        URL url = getUrl();
+        try (InputStream inputStream = url.openStream();
+            JsonReader reader = Json.createReader(inputStream)) {
+            JsonObject data = reader.readObject();
+            JsonObject entries = data.getJsonObject("entries");
+            songs = entries.getJsonArray("items");
         }
-        else {
-          return Optional.of(shape);
-        }
+        return songs;
     }
 
-    // defaults to 0
-    private int length;
-    private int width;
-
-    public SpotifyCharts(final int length, final int width) {
-        this.length = length;
-        this.width = width;
+    public String getName(song) {
+        return song.get("track").get("name");
     }
 
-    public int getLength() {
-        return length;
-    }
-
-    public int getWidth() {
-        return width;
-    }
-
-    public int getArea() {
-        return length * width;
+    public String getTopTwo() {
+        JsonArray songs = getSongs();
+        String songOne = getName(songs.getJsonObject(0));
+        String soneTwo = getName(songs.getJsonObject(1));
+        return "1. " + songOne + " 2. " + soneTwo;
     }
 }
+
--- src/test/java/SpotifyChartsTest.java
+++ src/test/java/SpotifyChartsTest.java
@@ -1,29 +1,51 @@
-import org.junit.Test;
+import org.junit.*;
 import static org.junit.Assert.*;
 import java.util.*;
 import com.google.common.collect.*;
+import java.io.*;
+import java.net.*;
+import javax.json.*;
 
 /*
  * SpotifyChartsTest
  */
 
 public class SpotifyChartsTest {
-    @Test public void testConstructor() {
-        SpotifyCharts instance = new SpotifyCharts(6,9);
-        assertEquals("constructor sets length as first argument", 6, instance.getLength());
-        assertEquals("constructor sets width as second argument", 9, instance.getWidth());
+    private SpotifyCharts charts;
+    @Before public void initialize() {
+        charts = new SpotifyCharts("US", 1);
     }
-    @Test public void testArea() {
-        SpotifyCharts instance = new SpotifyCharts(6,9);
-        assertEquals("getArea() returns the product of length and width", 54, instance.getArea());
+    @Test public void testGetCountry() {
+        assertEquals("getCountry returns country", "US",  charts.getCountry());
     }
-    @Test public void testGetShape() {
-        // Optional<String> shape = SpotifyCharts.getShape(3);
-        // if (shape.isPresent()) {
-        //     System.out.println(shape.get());
-        // }
-        assertEquals("SpotifyCharts.getShape() returns triangle w/param of 3", Optional.of("triangle"),  SpotifyCharts.getShape(3));
-        assertEquals("SpotifyCharts.getShape() returns pentagon w/param of 5", Optional.of("pentagon"),  SpotifyCharts.getShape(5));
-        assertEquals("SpotifyCharts.getShape() returns absent for shape not in dict", Optional.empty(),  SpotifyCharts.getShape(80));
+    @Test public void testGetRank() {
+        assertEquals("getRank returns rank", 1,  charts.getRank());
+    }
+    @Test public void testGetParams() {
+        String params = "type=regional&country=US&recurrence=daily&date=latest";
+        assertEquals("getParams formats params", params, charts.getParams());
+    }
+    @Test public void testGetStringUrl() {
+        String sUrl = "https://spotifycharts.com/api/?type=regional&country=US&recurrence=daily&date=latest";
+        assertEquals("getUrl adds params to url", sUrl, charts.getStringUrl());
+    }
+    @Test public void testGetUrl() {
+        URL url = new URL("https://spotifycharts.com/api/?type=regional&country=US&recurrence=daily&date=latest");
+        assertEquals("getUrl adds params to url", url, charts.getUrl());
+    }
+    @Test public void testGetSongs() {
+        JsonArray songs = charts.getSongs();
+        String songName = songs.get(0).get("track").get("name");
+        assertEquals("getSongs returns list of songs", "Panda", songName);
+    }
+    @Test public void getName() {
+        JsonObject firstSong = charts.getSongs().getJsonObject(0);
+        JsonObject secondSong = charts.getSongs().getJsonObject(1);
+        assertEquals("getName returns track name", "Panda", charts.getName(firstSong));
+        assertEquals("getName returns track name", "Work", charts.getName(secondSong));
+    }
+    @Test public void getTopTwo() {
+        String tracks = "1. Panda 2. Work";
+        assertEquals("getTopTwo returns string of first two songs", tracks, charts.topTwo());
     }
 }
